c7d298ba3eeb181e5b6675c96a1b9e60e2e10886
Khanh <uefa.euro30@gmail.com>
2021-08-17 16:43:22 +0700

    abc

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-let userName = prompt("Who's there?", '');
-if (userName == 'Admin') {
-    let pass = prompt('Password?', '');
-    if (pass == 'TheMaster') {
-        alert('Welcome!');
-    } else if (pass == null) {
-        alert('Canceled.');
-    } else {
-        alert('Wrong password');
-    }
-} else if (userName == null) {
-    alert('Canceled');
-} else {
-    alert("I don't know you");
-}
+// b1
+result = (a + b < 4) ? 'Below' : 'Over';
+// b2
+let message = (login == 'Employee') ?
+    'Hello' :
+    (login == 'Director') ?
+        'Greetings' :
+        (login == '') ?
+            'No login' :
+            '';
+alert(message)
